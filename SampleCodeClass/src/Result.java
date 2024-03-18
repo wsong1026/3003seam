3fad62600473f8c0264dda2f339eb847affa855b
limfj <39509427+limfj@users.noreply.github.com>
2024-03-18 08:20:01 +0800

    Add files via upload

---
+++ SampleCodeClass/src/Result.java
@@ -0,0 +1,34 @@
+public class Result {
+    String name;
+    
+    int RE;
+    int SDA;
+    int SQAT;
+    int SEAM;
+    int FM;
+   
+    double getAverageMark()
+    {
+        return (RE+SDA+SQAT+SEAM+FM)/4;
+    }
+
+  
+    int getTotal()
+    {
+        return (RE+SDA+SQAT+SEAM+FM);
+    }
+    
+    void testDisplay()
+    {
+        System.out.println(getTotal());
+
+    }
+   
+    void testAll()
+ 	{
+     		System.out.println("Hello");
+     		return;
+	}
+	
+
+}
