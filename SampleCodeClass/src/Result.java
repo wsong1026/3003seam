f6ede3cb11d1867fb28dcc6c6cda85a5178d470e
ellynne <100462181+ellynnehu@users.noreply.github.com>
2024-03-18 14:35:08 +0800

    Update Result.java

--- SampleCodeClass/src/Result.java
+++ SampleCodeClass/src/Result.java
@@ -1,14 +1,20 @@
 public class Result {
     String name;
-    
+
     int RE;
     int SDA;
     int SQAT;
     int SEAM;
     int FM;
     int HCI;
+
     double getAverageMark()
     {
+	RE = 1;
+	SDA = 1;
+	SQAT = 1;
+	SEAM = 1;
+	FM = 1
         return (RE+SDA+SQAT+SEAM+FM)/4;
     }
 
